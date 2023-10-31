84ac029d657b287b912a827c7e4f78968a816022
Misaelcs <misaelcsauceda@gmail.com>
2023-10-31 17:31:32 -0300

    1

--- teste-juscash/src/router.ts
+++ teste-juscash/src/router.ts
@@ -8,8 +8,6 @@ import { setupLayouts } from 'virtual:generated-layouts';
 export let extendedRoutes: any = null;
 export const router = createRouter({
 	history: import.meta.env.SSR ? createMemoryHistory() : createWebHistory(),
-	// You don't need to pass the routes anymore,
-	// the plugin writes it for you 🤖
 	extendRoutes: routes => {
 		extendedRoutes = routes;
 		return setupLayouts(routes);
